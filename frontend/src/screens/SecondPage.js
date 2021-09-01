9a21058057eabded3c46d6f5a880c2a39a322278
luigicavazzani <luigicavazzani@poli.ufrj.br>
2021-08-31 22:07:04 -0300

    changed SecondPage method

--- frontend/src/screens/SecondPage.js
+++ frontend/src/screens/SecondPage.js
@@ -1,63 +1,54 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import api from '../api'
-import InteractiveCard, {Header, Content} from 'react-native-interactive-card';
+import {SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import * as screen from "../constants/dimensions";
 
-export default class CustomTransition extends React.Component{
-	constructor() {
-		super();
-		this.state={
-			results: null
-		}
-	}
+
+export default function SecondPage() {
+	const [results, setResults] = useState([])
 	
-	render() {
-		
-		return (
-			<View style={styles.container}>
-				<InteractiveCard overlayOpacity={1}>
-					<Header>
-						<View style={styles.cardHeader}>
-							<Text style={styles.text}>Caixa d'água 1</Text>
-						</View>
-					</Header>
-					<Content enterFrom={"right"}>
-						<View style={styles.content1}>
-							<Text style={styles.text}>ttttt</Text>
-						</View>
-					</Content>
-				</InteractiveCard>
-				<InteractiveCard overlayOpacity={1}>
-					<Header>
-						<View style={styles.cardHeader}>
-							<Text style={styles.text}>Caixa d'água 2</Text>
-						</View>
-					</Header>
-					<Content enterFrom={"right"}>
-						<View style={styles.content2}>
-							<Text style={styles.text}>57%</Text>
-						</View>
-						<View style={styles.content3}>
-							<Text style={styles.text}>teste</Text>
-						</View>
-					</Content>
-				</InteractiveCard>
-			</View>
-		);
-	}
+	const getApiDados = async () => {
+		try {
+			const url = "http://app-pipa.herokuapp.com/status"
+			const response = await axios.get(url)
+			setResults(response.data.data);
+		} catch (e) {
+		alert("erro")
+		}
+	};
+
+	useEffect (() => {
+		getApiDados();
+	}, []);
+
+    return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+            <Text>Status</Text>   
+        </View>
+		<View style={styles.body}>
+			<Text>{results.sc1?"Cheio":"Vazio"}</Text> 
+		    <Text>{results.volume}</Text>
+    
+        </View>
+    
+    </SafeAreaView>  
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#87CEFA',
-		justifyContent: 'center',
-		padding: 10
-	},
-	cardHeader: {width:"100%", backgroundColor: "#68E9FF",padding: 30,marginBottom: 10, borderRadius: 5},
-	text: {fontSize: 40, opacity: 0.6,textAlign: 'center',fontWeight: 'bold'},
-	content1: {width: "85%", height: "90%", padding: 50, backgroundColor: "#54B8AC"},
-	content2: {width: "85%", padding: 50, backgroundColor: "#54B8AC"},
-	content3: {width: "85%", padding: 50, backgroundColor: "#C0C0C0"},
+  container: {
+    flex: 1,
+  },
+  header: {
+    marginTop: screen.height * 0.1,
+    alignItems: "center",
+  },
+
+  body: {
+    alignItems: "center",
+	marginTop: screen.height * 0.2,
+  },
+
 
 });
