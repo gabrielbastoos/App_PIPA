8d78de0175bd3230816961eb4cc4523cdd5947aa
Gabriel Bastos <gabriel.bastos@poli.ufrj.br>
2021-09-22 18:36:06 -0300

    tela de cadastro no app quase pronta, so falta validar post

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,16 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FirstPage from './screens/FirstPage';
 import SecondPage from './screens/SecondPage';
- 
+import RegisterPage from './screens/registerPage';
+
+
 const Stack = createNativeStackNavigator();
 
 export default function(){
     return(
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Cadastro" component={RegisterPage}/>
                 <Stack.Screen name="Home" component={FirstPage}/>
                 <Stack.Screen name="SecondPage" component={SecondPage}/>
             </Stack.Navigator>
---
+++ frontend/src/screens/registerPage.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet,TouchableOpacity, Text, View, KeyboardAvoidingView, TextInput } from 'react-native';
+import * as screen from "../constants/dimensions";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default function RegisterPage({navigation}) {
+  
+  const icon = !visible ? 'eye-slash' : 'eye';
+  //const [value, onChangeText] = React.useState(props.value);
+  const [visible, setVisibility] = React.useState(false);
+
+  const [credencial, setCredencial] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [uuid, setUUID] = React.useState('');
+  const [senha, setSenha] = React.useState('');
+  const [nome, setNome] = React.useState('');
+
+
+  async function Cadastrar() {
+
+    var jsonCadastro = {
+      "admin-credential": credencial,
+      "user-name": nome,
+      "uuid-pipa":uuid,
+      "user-email":email,
+      "user-password":senha
+    };
+
+    console.log(jsonCadastro);
+
+    // var resposta = "";
+    // await fetch('URL DE CADASTRO', {
+    //   method: 'POST',
+    //   body:jsonCadastro
+    // })
+    // .then((response) => response.text())
+    // .then((responseText) => {
+    //   resposta = responseText;
+    //   console.log('respostaCadastro:',resposta)
+      
+    // })
+    // .catch((error) => {
+    //   console.log(error)
+    // });
+
+    // //console.log(this.NotificationContainer.registerForPushNotificationsAsync);
+    //     registerForPushNotificationsAsync();
+
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+
+      <KeyboardAvoidingView behavior={'position'}>
+
+        <Text style={styles.cadastreText}>Cadastre-se</Text>
+
+        <Text style={styles.fieldCadastroTop}>Credencial de administrador</Text>
+        <TextInput 
+          style={styles.entradaTexto}
+          placeholder={"Digite a credencial"}
+          maxLength={5}
+          value={credencial}
+          onChangeText={(newCredencial) => setCredencial(newCredencial)}
+        />
+        <View
+          style={styles.linhaCampos}
+        />
+
+        <Text style={styles.fieldCadastro}>Nome do usuário</Text>
+        <TextInput 
+          style={styles.entradaTexto}
+          placeholder={"Nome Completo"}
+          maxLength={40}
+          value={nome}
+          onChangeText={(newNome) => setNome(newNome)}
+        />
+        <View
+          style={styles.linhaCampos}
+        />
+
+
+        <Text style={styles.fieldCadastro}>UUID do PIPA</Text>
+        <TextInput 
+          style={styles.entradaTexto}
+          placeholder={"Código do sensor"}
+          maxLength={10}
+          value={uuid}
+          onChangeText={(newUuid) => setUUID(newUuid)}
+        />
+        <View
+          style={styles.linhaCampos}
+        />
+
+        <Text style={styles.fieldCadastro}>E-mail</Text>
+        <TextInput 
+          style={styles.entradaTexto}
+          placeholder={"Digite o e-mail do usuário"}
+          maxLength={40}
+          value={email}
+          onChangeText={(newEmail) => setEmail(newEmail)}
+        />
+        <View
+          style={styles.linhaCampos}
+        />
+
+
+        <Text style={styles.fieldCadastro}>Senha</Text>
+        <TextInput
+          style={styles.entradaTexto}
+          value={senha}
+          onChangeText={(password) => setSenha(password)}
+          autoCapitalize={'none'}
+          placeholder={"Senha"}
+          secureTextEntry={!visible}
+        />
+        <View
+          style={styles.linhaCampos}
+        />
+        <Icon
+          name={icon}
+          color={'#9e9e9e'}
+          onPress={() => setVisibility(!visible)}
+          style={styles.icons}
+        />
+
+    </KeyboardAvoidingView>
+
+      <TouchableOpacity style={styles.submitButton} 
+        onPress={() => 
+        {console.log(navigation.navigate("Home"))
+        Cadastrar();
+        }}> 
+        
+        <Text style={styles.submitButtonText}>Finalizar</Text>
+
+      </TouchableOpacity>
+
+    </SafeAreaView>  
+     );
+}
+
+const styles = StyleSheet.create({
+
+  icons: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    height: screen.height*0.06,
+    width: screen.height*0.05,
+    marginLeft:screen.width*0.72,
+    marginTop:-screen.width*0.1
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff"
+  },
+  cadastreText:{
+    marginTop:screen.height*0.08,
+    marginLeft:screen.width*0.08,
+    fontFamily:"Quicksand",
+    fontWeight:"bold",
+    fontSize:26,
+    color:"#35424a"
+  },
+  linhaCampos: {
+    borderBottomColor: "#e8e4e3",
+    marginLeft:screen.width*0.08,
+    marginRight:screen.width*0.2,
+    marginTop:screen.width*0.02,
+    borderBottomWidth: 2,
+
+  },
+          
+  fieldCadastroTop:{
+    marginTop:screen.height*0.05,
+    marginLeft:screen.width*0.08,
+    //fontFamily:"Arial",
+    fontWeight:"bold",
+    fontSize:14,
+    color:"#f85f6a"
+  },
+
+  fieldCadastro:{
+    marginTop:screen.height*0.03,
+    marginLeft:screen.width*0.08,
+   // fontFamily:"Arial",
+    fontWeight:"bold",
+    fontSize:14,
+    color:"#f85f6a"
+  },
+
+  entradaTexto:{
+    marginTop:screen.height*0.02,
+    marginLeft:screen.width*0.08,
+    marginRight:screen.width*0.2,
+    paddingHorizontal: 1,
+   // fontFamily:"Arial",
+    fontWeight:"bold",
+    fontSize:12,
+    borderRadius:10,
+    color:"#35424a",
+  },
+
+  submitButton:{
+    width: screen.width *0.7,
+    height: screen.height * 0.08,
+    backgroundColor: "#0c59cf",
+    alignSelf: "center",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop:screen.height*0.03
+  },
+
+  submitButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 17,
+  }
+});
